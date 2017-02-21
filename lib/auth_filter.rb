e7474c8f287fa78bfe71ca7a01b72b11012c6c18
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-02-21 16:50:08 +0100

    added lets encrypt support
    On branch: master

--- lib/auth_filter.rb
+++ lib/auth_filter.rb
@@ -5,7 +5,9 @@ class AuthFilter
 
   def call(env)
     request = Rack::Request.new(env)
-    unless request.session[:authenticated] || request.path_info =~ /^\/(auth|pingdom)/
+    unless request.session[:authenticated] ||
+        request.path_info =~ /^\/(auth|pingdom)/ ||
+        request.path_info =~ /\/\.well-known\/acme-challenge\//
       request.session[:lgkp] = request.path
       url = request.scheme + "://" + request.host_with_port + "/auth/google_oauth2"
       return [ 307, { 'Location' => url }, []]
